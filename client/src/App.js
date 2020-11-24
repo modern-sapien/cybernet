7193cccf28f8ddfc5d5cc29227e99ba013f39340
Lexx <joncanales2@gmail.com>
2020-11-24 16:01:06 -0500

    scene, camera & renderer set

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import {useEffect} from "react"
 import axios from "axios"
 import './App.css';
+import MainGallery from "./containers/Main/Main"
 
 function App() {
 
-  useEffect(() => {
-    console.log("make an API call")
-    axios
-      .get("/api/config")
-      .then((response) => {
-        console.log(response)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+  // useEffect(() => {
+  //   console.log("make an API call")
+  //   axios
+  //     .get("/api/config")
+  //     .then((response) => {
+  //       console.log(response)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }, [])
 
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <MainGallery />
     </div>
   );
 }
---
+++ client/src/containers/Main/Main.jsx
@@ -0,0 +1,64 @@
+import React, {useEffect} from 'react';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const Main = () => {
+
+    useEffect(() => {
+    dimensions()
+    }, [])
+
+    function dimensions() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      100,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      5000
+    );
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor("#000000");
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    const container = document.getElementById("myCanvas"); 
+    container.appendChild(renderer.domElement);
+   
+    // this is updating the of the scene ever time
+    window.addEventListener("resize", () => {
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      camera.aspect = window.innerWidth / window.innerHeight;
+      // every time an adjustment is made on the camera this must be called
+      camera.updateProjectionMatrix();
+    });
+
+    let controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxDistance = 5000;
+
+    // start position cam
+    camera.position.set(0, 2800, 140);
+
+    // Base Scene Light
+    const baseLight = () => {
+    const light = new THREE.AmbientLight(0x404040, 1);
+    scene.add(light);
+    };
+
+    baseLight();
+    baseLight();
+    baseLight();
+
+    function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    }
+    animate();
+    
+    }
+
+
+    return (
+      <div id="myCanvas"></div>
+    );
+};
+
+export default Main;
