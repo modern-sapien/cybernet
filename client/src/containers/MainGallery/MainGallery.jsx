1b1a1d15fd6ab1e3b701007a2edec21a5dd6479e
Lexx <joncanales2@gmail.com>
2020-11-24 15:29:33 -0500

    imported three.js & main gallery images.

---
+++ client/src/containers/MainGallery/MainGallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import bk from "cyber_bk.png"
+import dn from "cyber_dn.png"
+import ft from "cyber_ft.png"
+import lf from "cyber_lf.png"
+import rt from "cyber_rt.png"
+import up from "cyber_up.png"
+
+const MainGallery = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MainGallery;
