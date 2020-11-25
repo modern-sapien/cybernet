3b1258f3ca221c39d2161a8f73403bc338ee013b
Lexx <joncanales2@gmail.com>
2020-11-25 16:44:20 -0500

    rough upload modal in place.

--- client/src/components/ImgUploadModal/ImgUploadModal.jsx
+++ client/src/components/ImgUploadModal/ImgUploadModal.jsx
@@ -1,23 +1,24 @@
-
 const ImgUploadModal = () => {
-    return (
-        <>
-        <div className="img-upload-modal row nomargin-nopadding">
-        <div className="col s3 m3 l3 "></div>
-        
-        <div className="col s6 m6 l6">
-        <form>
-            <input type="file" id="img" name="img" accept="image/*"/>
-            <input type="submit"/>
-        </form>
-        </div>
+  return (
+    <>
+      <div className="row img-upload-modal">
+          <div className="img-upload-form">
+          </div>
+          <form action="">
+           <input
+              className="col s8 m8 l8"
+              type="file"
+              id="img"
+              name="img"
+              accept="image/*"
+            />
+            <div className="col s4 m4 l4 btn" value="File Upload">File Upload </div>
+          </form>
+      </div>
 
-        <div className="col s3 m3 l3"></div>
-        </div>
-
-        <div className="img-upload-modal-background"></div>
-        </>
-    );
+      <div className="img-upload-modal-background"></div>
+    </>
+  );
 };
 
 export default ImgUploadModal;
