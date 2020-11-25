13f65cac75daf34d9839cb408f1a21cb05afc51a
Lexx <joncanales2@gmail.com>
2020-11-25 17:10:42 -0500

    img upload modal needs space for comment.

--- client/src/components/ImgUploadModal/ImgUploadModal.jsx
+++ client/src/components/ImgUploadModal/ImgUploadModal.jsx
@@ -1,10 +1,10 @@
 const ImgUploadModal = () => {
   return (
     <>
-      <div className="row img-upload-modal">
+      <div className="img-upload-modal row">
           <div className="img-upload-form">
-          </div>
-          <form action="">
+          
+          <form action="" className="">
            <input
               className="col s8 m8 l8"
               type="file"
@@ -12,8 +12,10 @@ const ImgUploadModal = () => {
               name="img"
               accept="image/*"
             />
-            <div className="col s4 m4 l4 btn" value="File Upload">File Upload </div>
+            <div className="col s4 m4 l4 form-btn" value="File Upload">
+            File Upload</div>
           </form>
+          </div>
       </div>
 
       <div className="img-upload-modal-background"></div>
