51cf78ad09e94e288ae53a97e2cb42b77f1013c0
Lexx <joncanales2@gmail.com>
2020-12-01 19:03:16 -0500

    beginning to work on routes & models.

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose')
-const path = require("path")
+const mongoose = require('mongoose');
+const path = require("path");
+// const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// ROUTES
+app.get('/', (req, res) =>  {
+  res.send("hello from express");
+})
+
+app.get('/api/v1/users', (req, res) =>  {
+  res.status(200).json({success: true, msg: "Show all users"});
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("client/build"));
+// app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cybernet", {
   useNewUrlParser: true,
@@ -34,9 +45,11 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
