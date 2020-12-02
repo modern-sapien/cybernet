7babeb3f8ef42730f1574c88cc36cdd09b65ad9e
Lexx <joncanales2@gmail.com>
2020-12-01 21:58:11 -0500

    get single user has basic error handling.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,29 +3,47 @@ const User = require("../models/User");
 // @desc    Get all users
 // @route   GET /api/v1/users
 // @access  Public
-exports.getUsers = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all users" });
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find()
+    
+    res.status(200).json({ success: true, data: users})
+  } catch (err) {
+    res.status(400).json({success:false})
+  }
 };
 
 // @desc    Get single user
 // @route   GET /api/v1/users/:id
 // @access  Public
-exports.getUser = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `display one user ${req.params.id}` });
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return  res.status(400).json({success:false})
+    }
+
+    res.status(200).json({success: true, data: user})
+  } catch (error) {
+    res.status(400).json({success:false})
+  }
 };
 
 // @desc    Create new user
 // @route   POST /api/v1/users/
 // @access  Public
 exports.createUser = async (req, res, next) => {
-  const user = await User.create(req.body);
+  try {
+    const user = await User.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    data: user
-  })
+    res.status(201).json({
+      success: true,
+      data: user
+    })
+  } catch (err) {
+    res.status(400).json({success: false})
+  }
 };
 
 // @desc    Update user
