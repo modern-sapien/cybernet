c55210227df6fb3a45bddc827c243d9389c5740a
Lexx <joncanales2@gmail.com>
2020-12-01 19:07:42 -0500

    basic user routes written.

--- server.js
+++ server.js
@@ -12,10 +12,27 @@ const app = express();
 app.get('/', (req, res) =>  {
   res.send("hello from express");
 })
-
+// GET ALL
 app.get('/api/v1/users', (req, res) =>  {
   res.status(200).json({success: true, msg: "Show all users"});
 });
+// GET ONE
+app.get('/api/v1/users/:id', (req, res) =>  {
+  res.status(200).json({success: true, msg: `display one user ${req.params.id}`});
+});
+// POST
+app.post('/api/v1/users', (req, res) =>  {
+  res.status(200).json({success: true, msg: "create new user"});
+});
+// UPDATE
+app.put('/api/v1/users/:id', (req, res) =>  {
+  res.status(200).json({success: true, msg: `display updated user ${req.params.id}`});
+});
+// Delete
+app.delete('/api/v1/users/:id', (req, res) =>  {
+  res.status(200).json({success: true, msg: `delete user ${req.params.id}`});
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
