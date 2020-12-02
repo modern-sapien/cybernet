c608872daa765b908da932616be8a4f911f9ef53
Lexx <joncanales2@gmail.com>
2020-12-01 19:28:05 -0500

    updated routes, and added a controller with headings.

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+// @desc    Get all users
+// @route   GET /api/v1/users
+// @access  Public
+exports.getUsers = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all users" });
+};
+
+// @desc    Get single user
+// @route   GET /api/v1/users/:id
+// @access  Public
+exports.getUser = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `display one user ${req.params.id}` });
+};
+
+// @desc    Create new user
+// @route   POST /api/v1/users/
+// @access  Public
+exports.createUser = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "create new user" });
+};
+
+// @desc    Update user
+// @route   PUT /api/v1/users/:id
+// @access  Private
+exports.updateUser = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `display updated user ${req.params.id}` });
+};
+
+// @desc    Delete user
+// @route   Delete /api/v1/users/:id
+// @access  Private
+exports.deleteUser = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `delete user ${req.params.id}` });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,15 @@
 const express = require("express");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 const router = express.Router();
 
-// GET ALL
-router.get('/', (req, res) =>  {
-res.status(200).json({success: true, msg: "Show all users"});
-});
-// GET ONE
-router.get('/:id', (req, res) =>  {
-res.status(200).json({success: true, msg: `display one user ${req.params.id}`});
-});
-// POST
-router.post('/', (req, res) =>  {
-res.status(200).json({success: true, msg: "create new user"});
-});
-// UPDATE
-router.put('/:id', (req, res) =>  {
-res.status(200).json({success: true, msg: `display updated user ${req.params.id}`});
-});
-// Delete
-router.delete('/:id', (req, res) =>  {
-res.status(200).json({success: true, msg: `delete user ${req.params.id}`});
-});
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
