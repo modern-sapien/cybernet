d0c5c90f004563c079bee99672b5a456e3730ba4
Lexx <joncanales2@gmail.com>
2020-12-06 20:16:14 -0500

    can update password for logged in users now.

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,6 +116,9 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    update details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  Private
 exports.updateDetails = asyncHandler(async (req, res, next) => {
   const fieldsToUpdate = {
     username: req.body.username,
@@ -128,6 +131,24 @@ exports.updateDetails = asyncHandler(async (req, res, next) => {
   });
 
   res.status(200).json({ success: true, data: user });
+});
+
+// @desc    update password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+
+    // Check current password
+    if(!(await user.matchPassword(req.body.currentPassword))){
+        return next(new ErrorResponse("Password is incorrect", 401));
+    }
+
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendTokenResponse(user, 200, res)
 });
 
 // Get token from model, create cookie & send response
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails } = require("../controllers/authController");
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require("../controllers/authController");
 const {protect} = require("../middleware/auth")
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 router.put("/updateDetails", protect, updateDetails);
+router.put("/updatePassword", protect, updatePassword);
 router.post("/forgotPassword", forgotPassword);
 router.put("/resetPassword/:resettoken", resetPassword);
 
