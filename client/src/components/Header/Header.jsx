f33a2ef5b031cded2105c54c4dbe56277928dc66
Lexx <joncanales2@gmail.com>
2020-12-13 22:07:33 -0500

    added temporary 3d model.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-        <Link to="/">
-        <div className="header-background col s2 m2 l2 ">VRAM</div>
-        </Link>
-        </>
-    );
+  return (
+    <>
+      <div className="header-background col s2 m2 l2 ">
+        <Link to="/">VRAM</Link>
+      </div>
+    </>
+  );
 };
 
 export default Header;
