43caf4576260adf952ce1a1b72399ae85f587fda
Lexx <joncanales2@gmail.com>
2021-02-10 17:28:54 -0500

    removin

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static("client/build"));
-app.use(express.static(path.join(__dirname, "/uploads")));
-app.use("/uploads", express.static("uploads"));
+// app.use(express.static(path.join(__dirname, "/uploads")));
+// app.use("/uploads", express.static("uploads"));
 app.use(fileupload());
 
 // Mount routers
